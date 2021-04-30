a8bad2cf37f8316a52dd93804caee3ef8aa251b4
AnneMarie Caballero <annemariecaballero@princeton.edu>
2021-04-30 03:06:05 -0400

    Populate with blocks (in progress

--- src/components/scenes/SeedScene.js
+++ src/components/scenes/SeedScene.js
@@ -4,6 +4,42 @@ import { Flower, Land } from 'objects';
 import { BasicLights } from 'lights';
 import * as THREE from 'three' // Probably need to change later, doing this for now for simplicity
 
+// let COLORS = [0x00916e, 0xfeefe5, 0xffcf00, 0xee6123, 0xfa003f];
+
+let COLORS = [0x00916e, 0xAEFFD8, 0xE44E5A, 0xFF990A, 0x6369D1];
+
+let populateWithBlocks = function(scene, numRows, minBricksPerRow, maxBricksPerRow, interval) {
+    
+    for (let i = 0; i < numRows; i++) {
+        let diff = maxBricksPerRow - minBricksPerRow; 
+
+        let numBricks = Math.floor(Math.random() * diff) + minBricksPerRow;
+        // REMOVE
+        numBricks = maxBricksPerRow;
+
+        let colorStart = Math.floor(Math.random() * COLORS.length);
+
+        for (let j = 0; j < maxBricksPerRow; j++) {
+            let colorIndex = (colorStart + j) % COLORS.length;
+
+            const platformGeom = new THREE.BoxGeometry(1.9, 0.45, 1);
+            const platformMat = new THREE.MeshPhongMaterial({ color: COLORS[colorIndex], flatShading: true });
+            const platformMesh = new THREE.Mesh(platformGeom, platformMat);
+
+            platformMesh.translateY((numRows - i) * 0.5 + interval);
+
+            let origin = -numBricks/2;
+
+            console.log(origin);
+
+            platformMesh.translateX(origin + 2 * j - 1.05);
+            console.log(origin + j);
+
+            scene.add(platformMesh);
+        }
+    }
+}
+
 class SeedScene extends Scene {
     constructor() {
         // Call parent Scene() constructor
@@ -28,7 +64,7 @@ class SeedScene extends Scene {
 
         // Add in a ball object (Note that this should probably go in the objects folder later, doing 
         // this here now for testing, same with the platform object)
-        const geometry = new THREE.SphereGeometry(0.7, 8, 8);
+        const geometry = new THREE.SphereGeometry(0.3, 8, 8);
         const material = new THREE.MeshPhongMaterial(
             {
                 color: 0x222222,
@@ -38,16 +74,15 @@ class SeedScene extends Scene {
         const sphere = new THREE.Mesh(geometry, material);
 
         // Add in a platform object
-        const platformGeom = new THREE.BoxGeometry(5, 1, 1);
-        const platformMat = new THREE.MeshPhongMaterial({ color: 0xdd1111, flatShading: true });
+        const platformGeom = new THREE.BoxGeometry(5, 0.5, 1);
+        const platformMat = new THREE.MeshPhongMaterial({ color: 0x00916e, flatShading: true });
         const platformMesh = new THREE.Mesh(platformGeom, platformMat);
         platformMesh.translateY(-2);
 
+        populateWithBlocks(this, 3, 1, 5, 2);
+
         this.add(lights, sphere, platformMesh);
-        console.log(platformMesh.position)
-
-        // this.add(land, flower, lights);
-
+        console.log(platformMesh.position);
 
         // Populate GUI (We probably don't want a GUI, at least not like this)
         // this.state.gui.add(this.state, 'rotationSpeed', -5, 5);
