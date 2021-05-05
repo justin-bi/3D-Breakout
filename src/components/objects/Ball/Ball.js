99f944b25104b9429386589ce34d08e09c8cb289
Justin Bi <justinbi247@gmail.com>
2021-05-04 23:19:56 -0400

    Beginning angle stuff, clean up and consider platform movement next

--- src/components/objects/Ball/Ball.js
+++ src/components/objects/Ball/Ball.js
@@ -14,14 +14,16 @@ class Ball extends Group {
         this.state = {
             // The direction of the ball, start by just going straight down
             vel: startVelocityVec,
+            dir: startVelocityVec.clone().normalize(),
+            speed: startVelocityVec.x / startVelocityVec.clone().normalize().x
         };
 
         const geometry = new THREE.SphereGeometry(radius, 8, 8);
+        console.log(this.state)
 
         // Build the vertices because three js is wack lmao
         // Want to store the vertices as Vector3s
         let vertices = [];
-        // this.mesh.geometry.attributes.position;
         for (let vi = 0; vi < geometry.attributes.position.count; vi++) {
             let vert = new THREE.Vector3(
                 geometry.attributes.position.array[vi * 3], 
@@ -31,6 +33,10 @@ class Ball extends Group {
             vertices.push(vert) 
         }
         this.verts = vertices;
+
+        // ANGLE code
+        this.minAngle = 30;
+        this.maxAngle = 60;
 
         const material = new THREE.MeshPhongMaterial(
             {
@@ -96,6 +102,7 @@ class Ball extends Group {
         // Code from here: https://stackoverflow.com/questions/11473755/how-to-detect-collision-in-three-js
         // Open to ideas, since this seems a tad complex
         let pos = this.mesh.position.clone();
+        let collisions = []
 
         for (let vi = 0; vi < this.verts.length; vi++) {
             // I think this doesn't actually transform anything, since this mesh was declared at the
@@ -105,9 +112,18 @@ class Ball extends Group {
             const rayCast = new THREE.Raycaster();
             rayCast.set(pos, dirVec.clone().normalize());
             let collisionResults = rayCast.intersectObjects(this.parent.children);
+            // Sort the results to handle the closest collision first, will give us best idea (hopefully)
 
             // If the ray collides with something, and the first collision (sorted in order of distance)
             // is less than the distance to the edge of the mesh itself, got a collision
+
+            if (collisionResults.length > 0 && collisionResults[0].distance < dirVec.length() ){
+                collisionResults[0].dirVec = dirVec.clone()
+                collisions.push(collisionResults[0])
+                continue;
+            }
+
+
             if (collisionResults.length > 0 && collisionResults[0].distance < dirVec.length()) {
 
                 // If it collided, first move it back to its position BEFORE the collisions, guaranteed to not be in a 
@@ -147,19 +163,57 @@ class Ball extends Group {
 
                     // This code makes sure that if the ball is colliding with the paddle, it ensures that the ball ends up 
                     // outside the paddle
-                    this.mesh.position.y = object.position.y + object.geometry.parameters.height + this.mesh.geometry.parameters.radius
+                    const rad = this.mesh.geometry.parameters.radius;
+                    const platWidth = object.geometry.parameters.width;
+                    const guaranteedHeight = object.position.y + (object.geometry.parameters.height / 2) + rad;
+                    if (this.mesh.position.y < guaranteedHeight) {
+                        this.mesh.position.y = guaranteedHeight;
+                    }
 
                     const ballX = this.mesh.position.x
                     const halfWidth = object.geometry.parameters.width / 2
                     const platX = object.position.x
-
                     const offset = (ballX - platX) / halfWidth  // Will be [-1, 1]
-                    // console.log(offset)
 
-                    // const platLeft = object.position.x - object.geometry.parameters.width
-                    // const platRight = object.position.x + object.geometry.parameters.width
-                    // console.log(ballX, platLeft, platRight, object)
-                    // console.log(object.position)
+                    // console.log(offset)
+                    let percent = 0.5;
+                    let newAngle;
+                    let onLeft;
+                    let movingLeft = object.leftPressed;
+                    let movingRight = object.rightPressed;
+                    let inMiddle = true;
+                    const cutoff = 0.5
+
+                    if (offset > cutoff) {
+                        percent = (offset - cutoff) * 2 // val in [0, 1], CURRENTLY hardcoded to work with * 2, figure out math later
+                        onLeft = false;
+                        inMiddle = false;
+                    } else if (offset < -cutoff) {
+                        percent = (offset + cutoff) * -2 // val in [0, 1]
+                        onLeft = true;
+                        inMiddle = false;
+                    }
+
+                    // If it's in the middle, nothing special needs to happen, can just use whatever happend in the
+                    // above general code. If it's not, need to ensure certain directions. 
+                    if (!inMiddle) {
+                        newAngle = this.maxAngle - (this.maxAngle - this.minAngle) * percent 
+                        newAngle = Math.max(Math.min(this.maxAngle, newAngle), this.minAngle)
+    
+                        // Now that we have the new angle, find the vector by converting this to radians
+                        let radAngle = newAngle * Math.PI / 180;    // Maybe have the angles be in rads from the start
+                        let newDir = new THREE.Vector3(1, 0, 0).applyAxisAngle(
+                            new THREE.Vector3(0, 0, 1),
+                            radAngle    
+                        )
+                        if (onLeft) {
+                            newDir.x *= -1
+                        }
+                        newDir.normalize()
+                        // Realllllll messy, will fix later -J
+                        this.state.vel = newDir.multiplyScalar(this.state.speed)
+                        console.log(newDir)
+                    }
                 }
 
                 // Do this hack for now
@@ -173,6 +227,113 @@ class Ball extends Group {
 
                 break;
             }
+        }
+        // if (ballI < 1 && collisions.length >= 3) {
+            
+        //     console.log(collisions)
+        //     ballI++
+        // }
+        if (collisions.length > 0) {
+                collisions.sort((a, b) => (a.distance > b.distance) ? 1 : -1)
+                // If it collided, first move it back to its position BEFORE the collisions, guaranteed to not be in a 
+                // collision at this point
+                this.mesh.position.sub(this.state.vel);
+
+                // The object the ball collides with
+                const object = collisions[0].object;
+
+                // NOTE! All the bounce code below might be able to be improved, since collision results return the face that 
+                // it collided with, meaning we can use the normals. This method still works, but should it fail, might want 
+                // to look into that switch
+
+                // Everything that the ball bounces off of is a box mesh (hopefully it stays that way haha),
+                // so to calculate which way its angle should go, find the angle of the dirVec that caused the 
+                // collision. Then, if it's within a certain range, cause it to reflect a certain way. 
+
+                 // Converting to degrees is easier
+                const angle = collisions[0].dirVec.clone().angleTo(new THREE.Vector3(1, 0, 0)) / Math.PI * 180
+
+                // First, handle cases where the ball collides mostly on the left or right
+                if (angle < 30 || (angle >= 150 && angle < 210) || angle >= 330) {
+                    this.state.vel.x *= -1;
+                }
+                // Otherwise, handle cases where it mostly bounces top or bottom
+                else if ((angle >= 60 && angle < 120) || (angle >= 240 && angle < 300)) {
+                    this.state.vel.y *= -1;
+                }
+                // Else, we assume it mostly bounced off a corner and reflect both
+                else {
+                    this.state.vel.y *= -1;
+                    this.state.vel.x *= -1;
+                }
+
+                if (object.name === "paddle") {
+                    // Note that it should do something special for the paddle, eg if it hits a certain area of 
+                    // the paddle it should change angle towards that direction, if the paddle is moving towards it
+                    // the ball should move in the same direction, etc. 
+
+                    // This code makes sure that if the ball is colliding with the paddle, it ensures that the ball ends up 
+                    // outside the paddle
+                    const rad = this.mesh.geometry.parameters.radius;
+                    const guaranteedHeight = object.position.y + (object.geometry.parameters.height / 2) + rad;
+                    if (this.mesh.position.y < guaranteedHeight) {
+                        this.mesh.position.y = guaranteedHeight;
+                    }
+
+                    const ballX = this.mesh.position.x
+                    const halfWidth = object.geometry.parameters.width / 2
+                    const platX = object.position.x
+                    const offset = (ballX - platX) / halfWidth  // Will be [-1, 1]
+
+                    // console.log(offset)
+                    let percent = 0.5;
+                    let newAngle;
+                    let onLeft;
+                    // let movingLeft = object.leftPressed;
+                    // let movingRight = object.rightPressed;
+                    let inMiddle = true;
+                    const cutoff = 0.5
+
+                    if (offset > cutoff) {
+                        percent = (offset - cutoff) * 2 // val in [0, 1], CURRENTLY hardcoded to work with * 2, figure out math later
+                        onLeft = false;
+                        inMiddle = false;
+                    } else if (offset < -cutoff) {
+                        percent = (offset + cutoff) * -2 // val in [0, 1]
+                        onLeft = true;
+                        inMiddle = false;
+                    }
+
+                    // If it's in the middle, nothing special needs to happen, can just use whatever happend in the
+                    // above general code. If it's not, need to ensure certain directions. 
+                    if (!inMiddle) {
+                        newAngle = this.maxAngle - (this.maxAngle - this.minAngle) * percent 
+                        newAngle = Math.max(Math.min(this.maxAngle, newAngle), this.minAngle)
+    
+                        // Now that we have the new angle, find the vector by converting this to radians
+                        let radAngle = newAngle * Math.PI / 180;    // Maybe have the angles be in rads from the start
+                        let newDir = new THREE.Vector3(1, 0, 0).applyAxisAngle(
+                            new THREE.Vector3(0, 0, 1),
+                            radAngle    
+                        )
+                        if (onLeft) {
+                            newDir.x *= -1
+                        }
+                        newDir.normalize()
+                        // Realllllll messy, will fix later -J
+                        this.state.vel = newDir.multiplyScalar(this.state.speed)
+                        console.log(newDir)
+                    }
+                }
+
+                // Do this hack for now
+                this.mesh.position.add(this.state.vel);
+
+                if (object.name === "brick") {
+                    this.parent.removeBrick(object.userData.brick);
+                } else if (object.name === "bottomBorder") {
+                    this.parent.handleBallHittingBottom(this);
+                }
         }
     }
 }
