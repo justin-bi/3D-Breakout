197feab018b2d5ef7a431b05d7e65575f3cd6c2f
AnneMarie Caballero <annemariecaballero@princeton.edu>
2021-05-05 20:54:04 -0400

    Make ball color of bricks it hits

--- src/components/objects/Brick/Brick.js
+++ src/components/objects/Brick/Brick.js
@@ -33,7 +33,7 @@ class Brick extends Group {
     breakBrick() {
         this.collidable = false;
         const shrink = new TWEEN.Tween(this.mesh.scale)
-        .to(new THREE.Vector3(0, 0, 0), 500)
+        .to(new THREE.Vector3(0, 0, 0), 300)
         .easing(TWEEN.Easing.Back.In);
 
         shrink.onComplete(() => {
